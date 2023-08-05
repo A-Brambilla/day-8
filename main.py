57c70afae7f7ae78450c269e8b86f66ee0a0ca36
A-Brambilla <austin.brambilla@outlook.com>
2023-08-05 03:36:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet():
+  print("Hello!")
+  print("I am a function.")
+  print("Isn't the weather nice today?")
+
+greet()
+
